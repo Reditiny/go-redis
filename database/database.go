e483229699e5c96d7f1d4e9fb55386fc8aaf3ce8
red <erickbin@stu.pku.edu.cn>
2023-10-15 14:54:23 +0800

    third commit 实现数据库内并发key-value安全结构以及key->ttl

--- database/database.go
+++ database/database.go
@@ -4,32 +4,48 @@ import (
 	"errors"
 	"fmt"
 	"redis-based-on-go/redis/protocol"
+	"time"
 )
 
 type DB struct {
-	table *concurrentMap // key -> value  value 为任意 reids 类型
+	kVTable  *concurrentMap // key -> value  value 为任意 reids 类型
+	ttlTable *concurrentMap // key -> ttl
 }
 
 func NewDB() *DB {
-	return &DB{table: makeConcurrentMap()}
+	return &DB{kVTable: makeConcurrentMap(), ttlTable: makeConcurrentMap()}
 }
 
 // setData 修改 db 的 key -> value value 为任意 redis 类型  内部修改由各结构本身实现
 func (db *DB) setData(key string, value interface{}) int {
-	_, ok := db.table.get(key)
+	_, ok := db.kVTable.get(key)
 	if ok {
-		db.table.delete(key)
-		db.table.set(key, value)
+		db.kVTable.delete(key)
+		db.kVTable.set(key, value)
 		return 0
 	} else {
-		db.table.set(key, value)
+		db.kVTable.set(key, value)
 		return 1
 	}
 }
 
+// setTTL 修改 db 的 key -> ttl
+func (db *DB) setTTL(key string, ttl time.Duration) {
+	db.ttlTable.set(key, time.Now().Add(ttl))
+}
+
 // getData 获取 指定 key 的 value
 func (db *DB) getData(key string) (interface{}, error) {
-	value, ok := db.table.get(key)
+	expire, ok := db.ttlTable.get(key)
+	if ok {
+		t := expire.(time.Time)
+		if t.Before(time.Now()) {
+			db.kVTable.delete(key)
+			db.ttlTable.delete(key)
+			return nil, errors.New(fmt.Sprintf("key '%s' has expired", key))
+		}
+	}
+	value, ok := db.kVTable.get(key)
 	if !ok {
 		return nil, errors.New(fmt.Sprintf("key '%s' not found", key))
 	} else {
@@ -39,7 +55,7 @@ func (db *DB) getData(key string) (interface{}, error) {
 
 // isExisted 判断指定 key 是否存在
 func (db *DB) isExisted(key string) bool {
-	_, ok := db.table.get(key)
+	_, ok := db.kVTable.get(key)
 	return ok
 }
 
